7efccae58405724312b690d5823ded5b8a984901
gyespinoza <74032012+gyespinoza@users.noreply.github.com>
2021-02-11 14:56:46 -0600

    Eventos, mouse y teclado 11/02/2021

--- proyectoejemplo/src/components/HelloWorld.vue
+++ proyectoejemplo/src/components/HelloWorld.vue
@@ -1,20 +1,28 @@
 <template>
-  <div class="hello">
-    <h1 v-color="'green'">{{ msg }}</h1>
-    
+  <div class="hello" v-on:mousemove="datosXY" @mouseover="mouseOver" @mouseout="mouseOut">
+    <h2>{{x}}, {{y}}</h2>
+    <h2>Mouse: {{msj}}</h2>
+    <h2><a v-on:click.prevent="clic" href="http://www.ugb.edu.sv">UGB</a></h2>
+
+    <h1 v-color="'green'">{{ msg }}</h1>    
 
     <!--a v-bind:href="ruta">Enlace</!--a-->
     <span v-html="mensajeHTML"></span>
     <h3 v-mensaje="'red'"></h3>
 
-    <b-form>
+    <b-form v-on:submit.prevent="agregarDato()"> 
         <b-row class="my-1">
             <b-col sm="3">
                 <label for="nombre">Escriba el nombre</label>
             </b-col>
             <b-col sm="3">
               <!--agregamos la directiva creada en la definicion del elemento-->
-                <b-form-input v-focus type="text" v-model="nombre" placeholder="Jose Flores"></b-form-input>
+                <b-form-input v-focus type="text" v-model="nombre" placeholder="Jose Flores"
+                @select="onSelect"
+                @cut="onCut"
+                @copy="onCopy"
+                @paste="onPaste"
+                ></b-form-input>
             </b-col>
         </b-row>
          <b-row class="my-1">
@@ -22,7 +30,7 @@
                 <label for="edad">Escriba la edad</label>
             </b-col>
             <b-col sm="3">
-                <b-form-input type="number" v-model="edad" placeholder="20"></b-form-input>
+                <b-form-input type="number" v-model="edad" placeholder="20" @keyup.alt.enter="agregarDato()" @keydown="key"></b-form-input>
             </b-col>
         </b-row>
          <b-row class="my-1">
@@ -39,13 +47,19 @@
       </b-form>
     <!--mostrar la data en un table-->
     <!--b-table striped hover :items="arreglo"></!--b-table-->
+    
     <b-table-simple>
       <b-thead>
           <b-th>ID</b-th>
           <b-th>Nombre</b-th>
           <b-th>Edad</b-th>
           <b-th>Sexo</b-th>
-          <b-th></b-th>
+
+          <!--eventos-->
+          <b-th><b-button variant="warning" v-on:click.once="arreglo=[]">Vaciar - click</b-button></b-th>
+          <b-th><b-button variant="warning" v-on:mousemove="arreglo=[]">Vaciar - mousemove</b-button></b-th>
+          <b-th><b-button variant="warning" v-on:dblclick="arreglo=[]">Vaciar - double click</b-button></b-th>    
+
       </b-thead>
       <b-tbody>
           <b-tr v-for="(dato, index) in arreglo" :key="dato.id">
@@ -69,7 +83,7 @@
               <b-td>{{ dato.sexo =='m'?'Masculino' : 'Femenino'}}</b-td>              
               <b-td>
                   <span v-if="actualizarF &&  idActualizar==index">
-                      <b-button variant="primary" v-on:click="guardarActualizacion(index)">Guardar</b-button>
+                      <b-button variant="primary" @click="guardarActualizacion(index)">Guardar</b-button>
                   </span>
                   <span v-else>
                       <b-button variant="success" v-on:click="actualizar(index)">Actualizar</b-button>
@@ -114,11 +128,43 @@ export default {
                 actualizarF:false,
                 mensajeHTML: `<h2><b>Ciclo I-2021</b></h2>
                   <b>INGENIERIA EN SISTEMAS Y REDES INFORMATICAS</b>
-                `
+                `,
+                x:0,
+                y:0,
+                msj:""
                 
     }  
   },
   methods:{
+    onSelect(){
+      alert('El texto ha sido seleccionado')
+    }, 
+    onCopy(){
+      alert('El texto ha sido copiado')
+    }, 
+    onPaste(){
+      alert('El texto ha sido pegado')
+    }, 
+    onCut(){
+      alert('El texto ha sido cortado')
+    },
+    key(event){
+      console.log(event.keyCode)
+    },
+    clic(){
+      alert('Se esta iniciando la carga')
+    },
+    mouseOver(){
+      this.msj="IN"
+    }, 
+    mouseOut(){
+      this.msj="OUT"
+    },
+      datosXY(event){
+       // console.log(event);
+        this.x=event.offsetX;
+        this.y=event.offsetY;
+      },
       agregarDato(){
                     this.arreglo.push({
                        id:this.id++, nombre: this.nombre, edad: this.edad, sexo: this.sexo
