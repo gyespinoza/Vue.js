f82b947d00304a60438fa00c39fc9c13dbc4e484
gyespinoza <74032012+gyespinoza@users.noreply.github.com>
2021-02-12 12:12:28 -0600

    Eventos de teclado, clases y estilos, 12/02/2021

--- proyectoejemplo/src/components/HelloWorld.vue
+++ proyectoejemplo/src/components/HelloWorld.vue
@@ -2,98 +2,132 @@
   <div class="hello" v-on:mousemove="datosXY" @mouseover="mouseOver" @mouseout="mouseOut">
     <h2>{{x}}, {{y}}</h2>
     <h2>Mouse: {{msj}}</h2>
-    <h2><a v-on:click.prevent="clic" href="http://www.ugb.edu.sv">UGB</a></h2>
-
-    <h1 v-color="'green'">{{ msg }}</h1>    
-
-    <!--a v-bind:href="ruta">Enlace</!--a-->
-    <span v-html="mensajeHTML"></span>
-    <h3 v-mensaje="'red'"></h3>
-
-    <b-form v-on:submit.prevent="agregarDato()"> 
-        <b-row class="my-1">
-            <b-col sm="3">
-                <label for="nombre">Escriba el nombre</label>
-            </b-col>
-            <b-col sm="3">
-              <!--agregamos la directiva creada en la definicion del elemento-->
-                <b-form-input v-focus type="text" v-model="nombre" placeholder="Jose Flores"
-                @select="onSelect"
-                @cut="onCut"
-                @copy="onCopy"
-                @paste="onPaste"
-                ></b-form-input>
-            </b-col>
-        </b-row>
-         <b-row class="my-1">
-            <b-col sm="3">
-                <label for="edad">Escriba la edad</label>
-            </b-col>
-            <b-col sm="3">
-                <b-form-input type="number" v-model="edad" placeholder="20" @keyup.alt.enter="agregarDato()" @keydown="key"></b-form-input>
-            </b-col>
-        </b-row>
-         <b-row class="my-1">
-            <b-col sm="3">
-                <label for="edad">Seleccione el sexo</label>
-            </b-col>      
-            <b-col sm="1">
-                <b-form-radio value="m" v-model="sexo">Masculino</b-form-radio>
-                <b-form-radio value="f" v-model="sexo">Femenino</b-form-radio>
-            </b-col>    
-        </b-row>
-
-        <b-button variant="primary" v-on:click="agregarDato()">Agregar Dato</b-button>
-      </b-form>
-    <!--mostrar la data en un table-->
-    <!--b-table striped hover :items="arreglo"></!--b-table-->
     
-    <b-table-simple>
-      <b-thead>
-          <b-th>ID</b-th>
-          <b-th>Nombre</b-th>
-          <b-th>Edad</b-th>
-          <b-th>Sexo</b-th>
+    <h1 :style="[estilo1, {'fontSize': fontSize+'px'}]">Ingenieria en Sistemas</h1>
+    <span>Font Size: {{ fontSize }}</span><br/>
 
-          <!--eventos-->
-          <b-th><b-button variant="warning" v-on:click.once="arreglo=[]">Vaciar - click</b-button></b-th>
-          <b-th><b-button variant="warning" v-on:mousemove="arreglo=[]">Vaciar - mousemove</b-button></b-th>
-          <b-th><b-button variant="warning" v-on:dblclick="arreglo=[]">Vaciar - double click</b-button></b-th>    
+    <button v-on:click="fontSize++">
+          Incrementar
+    </button>
+    <button v-on:click="fontSize--">
+          Decremento
+    </button>
 
-      </b-thead>
-      <b-tbody>
-          <b-tr v-for="(dato, index) in arreglo" :key="dato.id">
-            <b-td>{{dato.id}}</b-td>
-              <b-td>
-                <span v-if="actualizarF && idActualizar==index">
-                    <input type="text" v-model="nuevoNombre" class="form-class"/>
-                </span>
-                <span v-else>
-                    {{dato.nombre}}
-                </span>                    
-              </b-td>
-              <b-td>
-                 <span v-if="actualizarF &&  idActualizar==index">
-                    <input type="number" v-model="nuevaEdad" class="form-class"/>
-                </span>
-                <span v-else>
-                    {{dato.edad}}
-                </span>    
-              </b-td>              
-              <b-td>{{ dato.sexo =='m'?'Masculino' : 'Femenino'}}</b-td>              
-              <b-td>
-                  <span v-if="actualizarF &&  idActualizar==index">
-                      <b-button variant="primary" @click="guardarActualizacion(index)">Guardar</b-button>
+    <h2><a v-on:click.prevent="clic" href="http://www.ugb.edu.sv">UGB</a></h2>
+    <hr :style="{'background-color': 'black', 'height':'10px'}">
+
+    <h1>Cambiar apariencia</h1>
+    <b-form-group label="Fondo">
+      <b-form-radio v-model="fondo" value="bg-secondary text-white">bg-secondary</b-form-radio>
+      <b-form-radio v-model="fondo" value="bg-light">bg-light</b-form-radio>
+      <b-form-radio v-model="fondo" value="bg-dark text-white">bg-dark</b-form-radio>
+    </b-form-group>
+   <b-form-group label="Tamaño">
+      <b-form-radio v-model="tamanio" value="container">Center</b-form-radio>
+      <b-form-radio v-model="tamanio" value="w-50 p3">50%</b-form-radio>
+      <b-form-radio v-model="tamanio" value="w-75 p3">75%</b-form-radio>
+      <b-form-radio v-model="tamanio" value="w-100 p3">100%</b-form-radio>      
+    </b-form-group>
+
+    <!-- && , expresion de guardia-->
+    <!--div :class="[fondo ? 'bg-info' : 'bg-dark text-white']"-->    
+  <!--div :class="[fondo && 'bg-info']">-->    
+  <div :class="['bg-info', fondo, tamanio]">    
+      <h1 v-color="'green'">{{ msg }}</h1>    
+      <!--a v-bind:href="ruta">Enlace</!--a-->
+      <span v-html="mensajeHTML"></span>
+      <h3 v-mensaje="'red'"></h3>
+
+      <b-form v-on:submit.prevent="agregarDato()"> 
+          <b-row class="my-1">
+              <b-col sm="3">
+                  <label for="nombre">Escriba el nombre</label>
+              </b-col>
+              <b-col sm="3">
+                <!--agregamos la directiva creada en la definicion del elemento-->
+                  <b-form-input v-focus type="text" v-model="nombre" placeholder="Jose Flores"
+                  @select="onSelect"
+                  @cut="onCut"
+                  @copy="onCopy"
+                  @paste="onPaste"
+                  @focus="onFocus"
+                  @blur="lostFocus"
+                  ></b-form-input>
+              </b-col>
+          </b-row>
+          <b-row class="my-1">
+              <b-col sm="3">
+                  <label for="edad">Escriba la edad</label>
+              </b-col>
+              <b-col sm="3">
+                  <b-form-input type="number" v-model="edad" placeholder="20" @keyup.alt.enter="agregarDato()" @keydown="key"></b-form-input>
+              </b-col>
+          </b-row>
+          <b-row class="my-1">
+              <b-col sm="3">
+                  <label for="edad">Seleccione el sexo</label>
+              </b-col>      
+              <b-col sm="1">
+                  <b-form-radio value="m" v-model="sexo" >Masculino</b-form-radio>
+                  <b-form-radio value="f" v-model="sexo">Femenino</b-form-radio>
+
+                  <input type="radio" name="genero" checked @change="generoP" value="m">Masculino <br/>
+                  <input type="radio" name="genero" checked @change="generoP" value="f">Femenino
+              </b-col>    
+          </b-row>
+          <strong>Genero: {{genero}}</strong> <br/>
+
+          <b-button variant="primary" v-on:click="agregarDato()">Agregar Dato</b-button>
+        </b-form>
+      <!--mostrar la data en un table-->
+      <!--b-table striped hover :items="arreglo"></!--b-table-->
+      
+      <b-table-simple :class="[fondo]">
+        <b-thead>
+            <b-th>ID</b-th>
+            <b-th>Nombre</b-th>
+            <b-th>Edad</b-th>
+            <b-th>Sexo</b-th>
+
+            <!--eventos-->
+            <b-th><b-button variant="warning" v-on:click.once="arreglo=[]">Vaciar - click</b-button></b-th>
+            <b-th><b-button variant="warning" v-on:mousemove="arreglo=[]">Vaciar - mousemove</b-button></b-th>
+            <b-th><b-button variant="warning" v-on:dblclick="arreglo=[]">Vaciar - double click</b-button></b-th>    
+
+        </b-thead>
+        <b-tbody>
+            <b-tr v-for="(dato, index) in arreglo" :key="dato.id">
+              <b-td>{{dato.id}}</b-td>
+                <b-td>
+                  <span v-if="actualizarF && idActualizar==index">
+                      <input type="text" v-model="nuevoNombre" class="form-class"/>
                   </span>
                   <span v-else>
-                      <b-button variant="success" v-on:click="actualizar(index)">Actualizar</b-button>
-                      <b-button variant="danger" v-on:click="eliminar(index)">Eliminar</b-button>
+                      {{dato.nombre}}
+                  </span>                    
+                </b-td>
+                <b-td>
+                  <span v-if="actualizarF &&  idActualizar==index">
+                      <input type="number" v-model="nuevaEdad" class="form-class"/>
                   </span>
-              </b-td>
-          </b-tr>
-      </b-tbody>
-    </b-table-simple>
-
+                  <span v-else>
+                      {{dato.edad}}
+                  </span>    
+                </b-td>              
+                <b-td>{{ dato.sexo =='m'?'Masculino' : 'Femenino'}}</b-td>              
+                <b-td>
+                    <span v-if="actualizarF &&  idActualizar==index">
+                        <b-button variant="primary" @click="guardarActualizacion(index)">Guardar</b-button>
+                    </span>
+                    <span v-else>
+                        <b-button variant="success" v-on:click="actualizar(index)">Actualizar</b-button>
+                        <b-button variant="danger" v-on:click="eliminar(index)">Eliminar</b-button>
+                    </span>
+                </b-td>
+            </b-tr>
+        </b-tbody>
+      </b-table-simple>
+  </div>
   </div>
 </template>
 
@@ -114,7 +148,7 @@ export default {
                 idActualizar:-1,
                 nombre:'',
                 edad:'',
-                sexo:'',
+                sexo:'m',
                 arreglo: [],
                 nuevoNombre: '',
                 nuevaEdad: '',
@@ -131,11 +165,26 @@ export default {
                 `,
                 x:0,
                 y:0,
-                msj:""
+                msj:"",
+                genero:'m',
+                fondo:false,
+                tamanio:'w-100 p-3',
+                textDecoration: 'underline',
+                textWeight: 'bold',
+                fontSize: 40
                 
     }  
   },
   methods:{
+    generoP(e){
+      this.genero= e.target.value;
+    },
+    onFocus(){
+      //alert('Focus')
+    }, 
+    lostFocus(){
+      //alert('Lost Focus')
+    },
     onSelect(){
       alert('El texto ha sido seleccionado')
     }, 
@@ -188,6 +237,14 @@ export default {
         this.arreglo.splice(registro_id, 1);
      }
 
+},
+computed:{
+  estilo1(){
+      return{
+          textDecoration: this.textDecoration,
+          textWeight: this.textWeight
+      }
+  }
 },
   directives:{
     mensaje:{
