fdaf2a2f0fc116899a5f49e556a782983bd65618
gyespinoza <74032012+gyespinoza@users.noreply.github.com>
2021-02-18 14:34:55 -0600

    Semana 5 - Componentes

---
+++ semana 5/componentexample/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Header titulo="Programacion Computacional IV"></Header>
+    <Index titulo="Ejemplo Componentes"></Index>
+
+  </div>
+</template>
+
+<script>
+/*import HelloWorld from './components/HelloWorld.vue'
+import Ejemplo from './components/Ejemplo.vue'
+import Prueba from './components/Prueba.vue'*/
+import Index from './components/Index.vue'
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Index,
+    Header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
